9d907124cc53a6d18606f0c57b94675a1cb7ff31
Iliyana KEHAYOVA <iliyana.kehayova2407@gmail.com>
2025-05-14 10:25:54 +0200

    jalonblanc

--- Exercices/jalonblanc3.java
+++ Exercices/jalonblanc3.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
-public class jalonblanc2 {
+public class jalonblanc3 {
 public static void main(String[] args){
 
 
     Scanner conversion = new Scanner(System.in);
 
     System.out.println("Veuillez indiquer le prix HT. ");
-    int montant = conversion.nextLine();
+    int montant = conversion.nextInt();
 
     System.out.println("Veuillez indiquer le taux de TVA. ");
-    int tva = conversion.nextLine();
+    int tva = conversion.nextInt();
 
     int prixttc = montant+montant*tva/100;
 
