ecd02f44832cfefc1b8a9ad15134642a40bc8b67
Iliyana KEHAYOVA <iliyana.kehayova2407@gmail.com>
2025-06-16 16:29:59 +0200

    exo anglais avec fonction

--- excercices en anglais/exoanglais1.java
+++ excercices en anglais/exoanglais1.java
@@ -2,16 +2,29 @@
 import java.util.Scanner;
 
 public class exoanglais1 {
+
+public static boolean even (int entry, Scanner calculator){
+
+if(entry<0){
+    return entry+" est inférieur à 0.";}
+else if (entry==0){
+    return entry+" est égal à 0.";}
+else if (entry>0){
+    return entry+" est supérieur à 0.");};
+
+}
+
     public static void main(String[] args){
 
         Scanner calculator = new Scanner(System.in);
-
+ 
         System.out.println("Saisissez un chiffre.");
         int entry = calculator.nextInt();
-
+/*
 if(entry<0){System.out.println(entry+" est inférieur à 0.");}
 else if (entry==0){System.out.println(entry+" est égal à 0.");}
 else if (entry>0){System.out.println(entry+" est supérieur à 0.");};
 
+*/
 calculator.close();
 }}
